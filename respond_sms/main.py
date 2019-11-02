b5c164836590184abc55710fa4f11621617d1c71
Michaelselzer <michaelselzer28@gmail.com>
2019-11-02 18:55:49 -0500

    contains responder function

--- respond_sms/main.py
+++ respond_sms/main.py
@@ -1,15 +1,60 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+import os
 
 app = Flask(__name__)
 
+positiveResponse = ['yes', 'y', 'ye']
+negativeResponse = ['no', 'n']
+
+
 @app.route("/sms", methods=['GET', 'POST'])
-def replyPy(arg):
+def responder(arg):
+    text = MessagingResponse()  # prepare a response
+    data = request.values  # dict containing data about response
+    # extract the data we want
+    phoneNum = request.values.get('From')
+    body = request.values.get('Body').lower().strip()  # make lower and strip leading spaces
+    response = ""
+    if body in positiveResponse:
+        response = yesHandler()
+    elif body in negativeResponse:
+        response = noHandler()
+    else:
+        response = defaultHandler()
+    # text person back
+    text.message(response)
+    return str(text)
 
-    # Start our response
-    resp = MessagingResponse()
 
-    # Add a message
-    resp.message("Generic response to incoming text")
 
-    return str(resp)
+# how to add a response
+# resp = MessagingResponse()
+# # Add a message
+# resp.message(message here)
+
+
+# Handler functions here. Do the updating and stuff
+def yesHandler():
+    """
+    Updates DB for a Yes response for a user
+    :return: string to text back to user
+    """
+    return 'You replied yes'  # temporary
+
+
+def noHandler():
+    """
+    Updates DB for a No response, and does whatever else it needs to
+    :return: string to text back to user
+    """
+    return 'You replied no'  # temporary
+
+
+def defaultHandler():
+    """
+    Default handler if text is neither Yes or No. Probable sends the person back
+    their chore for the week
+    :return:
+    """
+    return 'Your chore for this week is _____'  # temporary
