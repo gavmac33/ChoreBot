d802af7e0ae133f86342dbfaf4028dd052f425d2
gavmac33 <gavin.m.macpherson@vanderbilt.edu>
2019-11-03 00:04:20 -0500

    Text users the correct chores for the new week

--- rotate_chore_wheel/main.py
+++ rotate_chore_wheel/main.py
@@ -1,24 +1,13 @@
 from twilio.rest import Client
 from google.cloud import bigquery
 import pandas
+import time
 
 def rotate_chore_wheel(arg):
     rows_df = read_chore_wheel()
     names, numbers, chores = calculate_rotation(rows_df)
-    df = remake_bq(names, numbers, chores)
-
-    account_sid = "ACa0d2e2a71c0083c170cda34c30576f01"
-    auth_token = "5b340f0856008eebda79cd94c7e983a0"
-    client = Client(account_sid, auth_token)
-
-    for index, row in rows_df.iterrows():
-        msg = str(row["name"]) + ", your chore for the new week is " + str(row["chore"])
-
-        client.messages.create(
-            to=str(row["number"]),
-            from_="16155517341",
-            body=msg
-        )
+    remake_bq(names, numbers, chores)
+    text_new_chores(names, numbers, chores)
 
 
 def read_chore_wheel():
@@ -63,11 +52,18 @@ def remake_bq(names, numbers, chores):
         query_job = bq_client.query(QUERY)  # API request
         query_job.result()  # Waits for query to finish
 
-    # Get updated chores wheel as dataframe
-    QUERY = "SELECT * FROM `chore-bot-257803.ChoreBot.choreWheel`"
 
-    bq_client = bigquery.Client()
-    query_job = bq_client.query(QUERY)  # API request
-    rows_df = query_job.result().to_dataframe()  # Waits for query to finish
+def text_new_chores(names, numbers, chores):
+    account_sid = "ACa0d2e2a71c0083c170cda34c30576f01"
+    auth_token = "5b340f0856008eebda79cd94c7e983a0"
+    client = Client(account_sid, auth_token)
 
-    return rows_df
+    for i in range(len(names)):
+        msg = names[i] + ", your chore for the new week is " + chores[i]
+
+        client.messages.create(
+            to=str(numbers[i]),
+            from_="16155517341",
+            body=msg
+        )
+
