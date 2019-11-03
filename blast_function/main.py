5fa0d53ebb73dd3713d0cd53b28553ed089bbd6d
gavmac33 <gavin.m.macpherson@vanderbilt.edu>
2019-11-03 01:20:57 -0500

    Added environ vars

--- blast_function/main.py
+++ blast_function/main.py
@@ -1,16 +1,17 @@
 from twilio.rest import Client
 from google.cloud import bigquery
 import pandas
+import os
 
 def blast_function(arg):
     rows_df = read_chore_wheel()
 
-    account_sid = "ACa0d2e2a71c0083c170cda34c30576f01"
-    auth_token = "5b340f0856008eebda79cd94c7e983a0"
+    account_sid = env_vars("ACCOUNT_SID")
+    auth_token = env_vars("AUTH_TOKEN")
     client = Client(account_sid, auth_token)
 
     for index, row in rows_df.iterrows():
-        msg = "ChoreBot REMINDER:" + str(row["name"]) + ", your chore this week is " + str(row["chore"])
+        msg = "ChoreBot REMINDER:  " + str(row["name"]) + ", your chore this week is " + str(row["chore"])
 
         client.messages.create(
             to=str(row["number"]),
@@ -26,4 +27,8 @@ def read_chore_wheel():
     rows_df = query_job.result().to_dataframe()  # Waits for query to finish
 
     return rows_df
+
+
+def env_vars(var):
+    return os.environ.get(var, 'Specified environment variable is not set.')
 
