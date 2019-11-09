6ca0f5be6231dbf81b5e0d5a6d3beb76f1e01cbf
gavmac33 <gavin.m.macpherson@vanderbilt.edu>
2019-11-09 15:55:03 -0600

    Refactored code, edited chorebot messages

--- respond_sms/main.py
+++ respond_sms/main.py
@@ -2,23 +2,19 @@ from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from google.cloud import bigquery
 import pandas
-import os
+
 
 app = Flask(__name__)
 
-positiveResponse = ['yes', 'y', 'ye']
+positiveResponse = ['yes', 'y', 'ye', 'complete']
 negativeResponse = ['no', 'n']
 helpResponse = ['option', 'o', 'op', 'opt']
 micromanageResponse = ['micromanage', 'micro', 'mic', 'm']
 statusResponse = ['status', 's', 'st', 'sta', 'stat']
 
 @app.route("/sms", methods=['GET', 'POST'])
-def responder(arg):
-    text = MessagingResponse()  # prepare a response
-    data = request.values  # dict containing data about response
-    # extract the data we want
-    phoneNum = data.get('From')
-    body = data.get('Body').lower().strip()  # make lower and strip leading spaces
+def responder(useless_arg):
+    phoneNum, body = parseApiCall(request.values) # dict containing data about response
 
     if body in positiveResponse:
         response = updateStatus(phoneNum)
@@ -34,14 +30,19 @@ def responder(arg):
         response = "I don't recognize that command." + help()
 
     # text person back
+    text = MessagingResponse()  # prepare a response
     text.message(response)
     return str(text)
-
 
 # how to add a response
 # resp = MessagingResponse()
 # # Add a message
 # resp.message(message here)
+
+def parseApiCall(data):
+    phoneNum = data.get('From')
+    body = data.get('Body').lower().strip()  # make lower and strip leading spaces
+    return phoneNum, body
 
 
 def noHandler():
@@ -49,7 +50,7 @@ def noHandler():
     Updates DB for a No response, and does whatever else it needs to
     :return: string to text back to user
     """
-    return "ChoreBot is sad that you haven't completed your chores yet. Please finish them by Sunday to make ChoreBot happy :("
+    return "ChoreBot: I'm sad that you haven't completed your chores yet. Please finish them by Sunday to make me happy :("
 
 
 def defaultHandler(phoneNum):
@@ -69,11 +70,11 @@ def defaultHandler(phoneNum):
     for index, row in rows_df.iterrows():
         if str(row["number"]) == phoneNum:
             if row["choreStatus"]:
-                return "You finished doing " + str(row["chore"]) + " already. ChoreBot is happy :)"
+                return "ChoreBot: You finished doing " + str(row["chore"]) + " already. I am very happy :)"
             else:
-                return "Your chore for this week is " + str(row["chore"]) + ". Please finish it by Sunday or ChoreBot will be angry!"
+                return "ChoreBot: Your chore for this week is " + str(row["chore"]) + ". Please finish it by Sunday or I will be angry!"
 
-    return "Error: ChoreBot is unable to find your chore"
+    return "ChoreBot: Error- I am unable to find your chore"
 
 
 def updateStatus(phoneNum):
@@ -82,22 +83,23 @@ def updateStatus(phoneNum):
     their chore for the week
     :return:
     """
-
     QUERY = "UPDATE `chore-bot-257803.ChoreBot.choreWheel` SET choreStatus = TRUE WHERE number = '" + phoneNum + "'"
 
     bq_client = bigquery.Client()
     query_job = bq_client.query(QUERY)  # API request
     query_job.result()  # Waits for query to finish
 
-    return "ChoreBot thanks you for finishing your chores!"
+    return "ChoreBot: Thank you for finishing your chores!"
 
 
 def help():
     return '''
-    ChoreBot offers the following options:
-    option(O): View chorebot options
+    ChoreBot: 
+    I respond to the following options:
+    option(O): View my options
     micromange(M): See who has and hasn't completed their chores
     status(S): Remind yourself what your chore is, and whether you have completed it
+    complete: Mark your chore as completed
     '''
 
 def micromanage():
