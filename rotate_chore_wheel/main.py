47a083976a9122e3dac9670f694a52120d795169
gavmac33 <gavin.m.macpherson@vanderbilt.edu>
2019-11-09 17:24:07 -0600

    Updated rotate_wheel to work with multiple groups

--- rotate_chore_wheel/main.py
+++ rotate_chore_wheel/main.py
@@ -1,13 +1,23 @@
 from twilio.rest import Client
 from google.cloud import bigquery
+from collections import deque, defaultdict
 import pandas
 import os
 
+class Group:
+    def __init__(self):
+        self.names = []
+        self.numbers = []
+        self.chores = deque()
+
+    def rotate_chores(self):
+        self.chores.append(self.chores.popleft())
+
 def rotate_chore_wheel(arg):
     rows_df = read_chore_wheel()
-    names, numbers, chores = calculate_rotation(rows_df)
-    remake_bq(names, numbers, chores)
-    text_new_chores(names, numbers, chores)
+    groups = calculate_rotation(rows_df)
+    rebuild_bq(groups)
+    text_new_chores(groups)
 
 
 def read_chore_wheel():
@@ -21,52 +31,64 @@ def read_chore_wheel():
 
 
 def calculate_rotation(rows_df):
-    from collections import deque
-    names = []
-    numbers = []
-    chores = deque()
+    groups = defaultdict(lambda: Group())
 
     for index, row in rows_df.iterrows():
-        names.append(str(row["name"]))
-        numbers.append(str(row["number"]))
-        chores.append(str(row["chore"]))
+        groupName = str(row["groupName"])
+        group = groups[groupName]
+
+        group.names.append(str(row["name"]))
+        group.numbers.append(str(row["number"]))
+        group.chores.append(str(row["chore"]))
 
     # Rotate chores
-    chores.append(chores.popleft())
+    for group in groups:
+        groups[group].rotate_chores()
 
-    return names, numbers, chores
+    return groups
 
 
-def remake_bq(names, numbers, chores):
-    for name in names:
-        QUERY = "DELETE FROM `chore-bot-257803.ChoreBot.choreWheel` WHERE name = '" + name + "';"
-
-        bq_client = bigquery.Client()
-        query_job = bq_client.query(QUERY)  # API request
-        query_job.result()  # Waits for query to finish
-
-    for i in range(len(names)):
-        QUERY = "INSERT INTO `chore-bot-257803.ChoreBot.choreWheel` (name, number, chore, choreStatus) SELECT '" + names[i] + "','" + numbers[i] + "','" + chores[i] + "', FALSE;"
+def rebuild_bq(groups):
+    # Change database one group at a time
+    for groupName in groups:
+        names = groups[groupName].names
+        numbers = groups[groupName].numbers
+        chores = groups[groupName].chores
 
         bq_client = bigquery.Client()
-        query_job = bq_client.query(QUERY)  # API request
-        query_job.result()  # Waits for query to finish
+
+        for name in names:
+            QUERY = "DELETE FROM `chore-bot-257803.ChoreBot.choreWheel` WHERE name = '" + name + "';"
+
+            query_job = bq_client.query(QUERY)  # API request
+            query_job.result()  # Waits for query to finish
+
+        for i in range(len(names)):
+            QUERY = "INSERT INTO `chore-bot-257803.ChoreBot.choreWheel` (name, number, chore, choreStatus, groupName) SELECT '%s','%s','%s', FALSE, '%s';"\
+                    % (names[i], numbers[i], chores[i], groupName)
+
+            query_job = bq_client.query(QUERY)  # API request
+            query_job.result()  # Waits for query to finish
 
 
-def text_new_chores(names, numbers, chores):
+def text_new_chores(groups):
     account_sid = env_vars("ACCOUNT_SID")
     auth_token = env_vars("AUTH_TOKEN")
     client = Client(account_sid, auth_token)
 
-    for i in range(len(names)):
-        msg = names[i] + ", your chore for the new week is " + chores[i]
+    for group in groups:
+        currentGroup = groups[group]
+        for i in range(len(currentGroup.names)):
+            msg = "ChoreBot: %s, your chore for the new week is: %s"  % (currentGroup.names[i], currentGroup.chores[i])
 
-        client.messages.create(
-            to=str(numbers[i]),
-            from_="16155517341",
-            body=msg
-        )
+            client.messages.create(
+                to=str(currentGroup.numbers[i]),
+                from_="16155517341",
+                body=msg
+            )
 
 
 def env_vars(var):
     return os.environ.get(var, 'Specified environment variable is not set.')
+
+
